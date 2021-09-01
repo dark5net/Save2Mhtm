b132139e806d333bdf273306111bac52ab1ec81f
Team dark5 <908869236@qq.com>
2021-09-01 01:26:55 -0700

    Initial commit

---
+++ Save2Mhtm/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Save2Mhtm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tBoxURL = new System.Windows.Forms.TextBox();
+            this.tBoxfilePath = new System.Windows.Forms.TextBox();
+            this.btnStartSave = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tBoxURL
+            // 
+            this.tBoxURL.Location = new System.Drawing.Point(12, 62);
+            this.tBoxURL.Multiline = true;
+            this.tBoxURL.Name = "tBoxURL";
+            this.tBoxURL.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tBoxURL.Size = new System.Drawing.Size(685, 326);
+            this.tBoxURL.TabIndex = 0;
+            this.tBoxURL.Text = "https://payloads.online/archivers/2018-12-19/1/";
+            // 
+            // tBoxfilePath
+            // 
+            this.tBoxfilePath.Location = new System.Drawing.Point(12, 23);
+            this.tBoxfilePath.Name = "tBoxfilePath";
+            this.tBoxfilePath.Size = new System.Drawing.Size(352, 20);
+            this.tBoxfilePath.TabIndex = 1;
+            // 
+            // btnStartSave
+            // 
+            this.btnStartSave.Location = new System.Drawing.Point(410, 21);
+            this.btnStartSave.Name = "btnStartSave";
+            this.btnStartSave.Size = new System.Drawing.Size(75, 23);
+            this.btnStartSave.TabIndex = 2;
+            this.btnStartSave.Text = "开始保存";
+            this.btnStartSave.UseVisualStyleBackColor = true;
+            this.btnStartSave.Click += new System.EventHandler(this.btnStartSave_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(540, 30);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 400);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnStartSave);
+            this.Controls.Add(this.tBoxfilePath);
+            this.Controls.Add(this.tBoxURL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Save2Mhtm beta 1 http://www.dark5.net";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBoxURL;
+        private System.Windows.Forms.TextBox tBoxfilePath;
+        private System.Windows.Forms.Button btnStartSave;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Save2Mhtm/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Save2Mhtm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartSave_Click(object sender, EventArgs e)
+        {
+            new Thread(() => this.Save2Mhtm()) { IsBackground = true }.Start();
+            btnStartSave.Text = "正在操作";
+        }
+        
+
+
+        private void Save2Mhtm()
+        {
+            int count = 0;
+
+            string url = "";
+            string titleStr = "";
+            string path = "";
+
+            //设定要下载到的路径
+            if (tBoxfilePath.Text.Trim() == "" || !Directory.Exists(tBoxfilePath.Text.Trim()))
+            {
+                path = "downloads";
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+            }
+            else { path = tBoxfilePath.Text.Trim(); }
+
+
+            //遍历url，下载mhtml
+            string[] lines = tBoxURL.Lines; //保存行，并记录，等待使用输出
+
+            //int countall = 0;
+            int counterror = 0;
+            for (int i = 0; i < tBoxURL.Lines.Length; i++)
+            {
+                url = tBoxURL.Lines[i].ToString();
+
+
+                titleStr = GetTitle(url);
+
+                if (titleStr == "")
+                {
+                    titleStr = count.ToString() + ".mhtml";
+                }
+                string fileSaveName = Path.Combine(path, titleStr+".mhtml");
+
+                Chilkat.Mht mht = new Chilkat.Mht();
+
+                bool mhtStr = mht.GetAndSaveMHT(url, fileSaveName);
+                if (mhtStr == true)
+                {
+                    string success_url = url + "    -- " + titleStr + "  -- ok";
+                    lines[i] = success_url;
+                }
+                else { string error_url = url + "  !!!Save Error!!!"; lines[i] = error_url; counterror++; }
+            }            
+            tBoxURL.Lines = lines;
+            btnStartSave.Text = "开始保存";
+            lbl1.Text = "总计：" + tBoxURL.Lines.Length.ToString() + "  错误：" + counterror.ToString();
+
+
+
+        }
+
+        private string GetTitle(string url)
+        {
+            string title = "";
+
+            WebClient client = new WebClient();
+            client.Headers["User-Agent"] = "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)";
+            Byte[] pageData = client.DownloadData(url);
+            string pageHtml = Encoding.UTF8.GetString(pageData);
+
+            Regex regx = new Regex("<title>(.*?)</title>");
+
+            try
+            {
+                string titleregx = regx.Match(pageHtml).Value; //<title>这是标题</title>
+                string titleClear = titleregx.Substring(7, titleregx.Length - 7 - 8);
+                string[] dd ={"◎", "■", "●", "№", "↑", "→", "↓" +
+"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "|", "[", "]", "？", "~", "`", "," +
+"！", "<", ">", "‰", "→", "←", "↑", "↓", "¤", "§", "＃", "＆", "＆", "＼", "≡", "≠" +
+"≈", "∈", "∪", "∏", "∑", "∧", "∨", "⊥", "‖", "‖", "∠", "⊙", "≌", "≌", "√", "∝", "∞", "∮" +
+"∫", "≯", "≮", "＞", "≥", "≤", "≠", "±", "＋", "÷", "×", "/" +
+"╄", "╅", "╇", "┻", "┻", "┇", "┭", "┷", "┦", "┣", "┝", "┤", "┷", "┷", "┹", "╉", "╇", "【", "】" +
+"┌", "├", "┬", "┼", "┍", "┕", "┗", "┏", "┅", "—" +
+"〖", "〗", "←", "〓", "☆", "§", "□", "‰", "◇", "＾", "＠", "△", "▲", "＃", "℃", "※", "≈", "￠","/"};
+                for (int b = 0; b < dd.Length; b++)
+                {
+                    string tt = dd[b];
+                    titleClear = titleClear.Replace(tt, "");
+
+                }
+
+                return titleClear;
+            }
+            catch
+            { return title; }
+                       
+        }
+
+
+    }
+}
